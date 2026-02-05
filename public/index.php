a634ea6fffafc7a86b3a7afc16dd59cbb0356045
Maxens LENORMAND <m.lenormand@dogglabel.local>
2026-02-05 16:16:20 +0100

    Mise à jour du site : ajout du public et includes

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+// Pour inclure le header 
+require_once __DIR__ . '/../includes/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lafeecaramel</title>
+    <link rel="stylesheet" href="../assets/css/layout.css">
+    <link rel="stylesheet" href="../assets/css/pages.css">
+</head>
+<body>
+
+
+
+<?php
+//Pour inclure le footer
+require_once __DIR__ . '/../includes/footer.php';
+?>
+<script src="../assets/js/app.js"></script>
+</body>
+</html>
