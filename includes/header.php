47be59b2e03746597eb8b7cd01851d635fd016b4
Maxens LENORMAND <m.lenormand@dogglabel.local>
2026-02-06 15:45:53 +0100

    Mise à jours du site (ajout du footer)

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<header>
+    <nav id="navbar">
+        <div class="container">
+            <div class="logo">
+                <a class="logo-init" href="#hero">LC</a>
+                <div class="logo-text">
+                    <a class="logo-titre" href="#hero">lafeecaramel</a>
+                    <a class="logo-desc" href="#hero">Soin & produits naturels</a>
+                </div>
+            </div>
+            <ul class="nav-links">
+                <li><a href="#hero">Acceuil</a></li>
+                <li><a href="#prestations">Prestations</a></li>
+                <li><a href="#produits">Produits</a></li>
+                <li><a href="#galerie">Galerie</a></li>
+                <li><a href="#apropos">À propos</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>            
+        </div>
+    </nav>
+</header>
--- public/index.php
+++ public/index.php
@@ -9,17 +9,26 @@ require_once __DIR__ . '/../includes/header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>lafeecaramel</title>
-    <link rel="stylesheet" href="../assets/css/layout.css">
-    <link rel="stylesheet" href="../assets/css/pages.css">
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/layout.css">
+    <link rel="stylesheet" href="./assets/css/pages.css">
 </head>
 <body>
 
-
+    <section id="hero">
+        <div class="container">
+            <h1>Bienvenue chez <span class="highlight">la fée caramel</span></h1>
+            <p>Découvrez nos délicieuses créations sucrées et laissez-vous tenter par nos gourmandises !</p>
+            <a href="#prestations" class="btn">Nos prestations</a>
+            <a href="#produits" class="btn">Nos produits</a>
+            <img src="./assets/images/hero-image.jpg" alt="Image de la fée caramel" class="hero-image">
+        </div>
+    </section>
 
 <?php
 //Pour inclure le footer
 require_once __DIR__ . '/../includes/footer.php';
 ?>
-<script src="../assets/js/app.js"></script>
+<script src="./assets/js/app.js"></script>
 </body>
 </html>
